7cc144559888a16c3da1f9af4edb69a50c9f5655
Abdallah Magdy <Abdallah.Magdy@natco-sae.com>
2024-02-11 19:44:38 +0200

    Add project files.

---
+++ Vacation.Models/Vacation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vacation.Models
+{
+    public class Vacation
+    {
+        public int Id { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        [Required,Display(Name="Employee Name")]
+        public string? EmployeeName { get; set; }
+        [Required]
+        public string? Title { get; set;}
+        [Required]
+        public DateTime VacationDateFrom { get; set; }
+        [Required]
+        public DateTime VacationDateTo { get; set; }
+        public DateTime Returning { get; set; }
+        public int TotalNumberOfDaysRequested { get; set; }
+
+        public string? Notes { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Department? Department { get; set; }
+
+
+
+
+
+
+    }
+}
